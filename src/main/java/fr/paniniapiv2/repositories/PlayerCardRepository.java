f6c2248f7e593964890cd686399195655427115a
Maxime Catteau <maxime.catteau13@gmail.com>
2021-08-26 00:26:04 +0200

    player career and ladder fix

--- src/main/java/fr/paniniapiv2/repositories/PlayerCardRepository.java
+++ src/main/java/fr/paniniapiv2/repositories/PlayerCardRepository.java
@@ -15,7 +15,7 @@ public interface PlayerCardRepository extends JpaRepository<PlayerCard, Integer>
     Optional<PlayerCard> findByCardIdAndPlayerId(int cardId, Long playerId);
 
     @Query(nativeQuery = true, value = "select count(*) from player_card pc inner join card c on pc.card_id = c.id where player_id = :playerId and c.collection_id = :collectionId")
-    Integer getNumberOfPlayerCardsOnCollection(Long playerId, int collectionId);
+    Integer getNumberOfPlayerCardsOnCollection(@Param("playerId") Long playerId, @Param("collectionId") int collectionId);
 
     @Query(nativeQuery = true, value = "select * from player_card pc where player_id = :playerId and quantity > 1;")
     List<PlayerCard> getPlayerDoubles(@Param("playerId") Long playerId);
